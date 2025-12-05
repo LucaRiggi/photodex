e275a8acdd937960b74cd7f7a1eab53544c8f615
Stanislav Nehretskyi <stanislavnegreckiy@gmail.com>
2025-12-05 13:51:28 -0500

    Add unit test for Network view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,4 +102,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(kotlin("test"))
+    testImplementation(libs.mockito.kotlin)
 }
